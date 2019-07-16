7e469c60678d0df02bf4294bbf94fe2208db16cc
Guillaume Bedard <bgui34@gmail.com>
2019-07-16 17:02:39 -0400

    First pass

---
+++ script.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest();
+
+request.open(
+  "GET",
+  "https://coderadio-admin.freecodecamp.org:8010/status-json.xsl",
+  true
+);
+
+request.onreadystatechange = e => {
+  console.log(request.responseText);
+  if (request.readyState == 4 && request.status == 200) {
+    var response = JSON.parse(request.responseText);
+    //alert(response.icestats.source[0].artist);
+    document.getElementById("artist").innerHTML =
+      response.icestats.source[0].artist;
+    document.getElementById("song").innerHTML =
+      response.icestats.source[0].yp_currently_playing;
+  }
+};
+
+request.send();
